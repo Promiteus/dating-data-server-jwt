fbb6cb837b41ec3f3fd65bb935d4eae6112ee5e8
Roman Matveev <r.matveev@socrat.marketing>
2021-11-22 16:29:41 +1000

    22.11.2021 -- Добавление сервиса для постраничеого отбора посетителей.

--- src/main/java/com/romanm/jwtservicedata/repositories/VisitorPagebleRepository.java
+++ src/main/java/com/romanm/jwtservicedata/repositories/VisitorPagebleRepository.java
@@ -3,8 +3,10 @@ package com.romanm.jwtservicedata.repositories;
 import com.romanm.jwtservicedata.models.Visitor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.reactive.ReactiveSortingRepository;
+import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
 
+@Repository("visitorPagebleRepository")
 public interface VisitorPagebleRepository extends ReactiveSortingRepository<Visitor, String> {
     Flux<Visitor> findVisitorByUserIdOrderByTimestampDesc(String userId, Pageable page);
 }
---
+++ src/main/java/com/romanm/jwtservicedata/services/VisitorServiceV1.java
@@ -0,0 +1,27 @@
+package com.romanm.jwtservicedata.services;
+
+import com.romanm.jwtservicedata.models.Visitor;
+import com.romanm.jwtservicedata.repositories.VisitorPagebleRepository;
+import com.romanm.jwtservicedata.services.interfaces.IVisitorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+
+@Service
+public class VisitorServiceV1 implements IVisitorService {
+
+    @Qualifier("visitorPagebleRepository")
+    private final VisitorPagebleRepository visitorPagebleRepository;
+
+    @Autowired
+    public VisitorServiceV1(VisitorPagebleRepository visitorPagebleRepository) {
+        this.visitorPagebleRepository = visitorPagebleRepository;
+    }
+
+    @Override
+    public Flux<Visitor> findPagebleVisitorsByUserId(String userId, int page, int pageSize) {
+        return this.visitorPagebleRepository.findVisitorByUserIdOrderByTimestampDesc(userId, PageRequest.of(page, pageSize));
+    }
+}
