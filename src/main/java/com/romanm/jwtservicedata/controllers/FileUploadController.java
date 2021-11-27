5d14c6b07d59eb8c1419cc4c8127cede768b0a0e
Roman Matveev <r.matveev@socrat.marketing>
2021-11-27 12:11:57 +1000

    27.11.2021 -- Создание контроллера получения и сохранения изображений. Часть 2. Переработка основных методов контроллера. Переименование контроллера.

---
+++ src/main/java/com/romanm/jwtservicedata/controllers/FileUploadController.java
@@ -0,0 +1,85 @@
+package com.romanm.jwtservicedata.controllers;
+
+import com.romanm.jwtservicedata.constants.Api;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.codec.multipart.FilePart;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@RestController
+@RequestMapping(value = Api.API_PREFIX)
+public class FileUploadController {
+
+
+    /**
+     * Получить все изображения пользователя по userId
+     * @param userId String
+     * @return Mono<ResponseEntity<Flux<Resource>>>
+     */
+    @GetMapping(value = Api.API_USER_IMAGES_USER_ID)
+    public Mono<ResponseEntity<Flux<Resource>>> getImages(@RequestParam(value = Api.PARAM_USER_ID, defaultValue = "", required = true) String userId) {
+
+        return Mono.create(sink -> {
+            sink.success(ResponseEntity.ok().build());
+        });
+    }
+
+    /**
+     * Сохранить файл
+     * @param userId String
+     * @param file Mono<FilePart>
+     * @return Mono<ResponseEntity<?>>
+     */
+    @PostMapping(value = Api.API_USER_IMAGES, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public Mono<ResponseEntity<?>> saveImage(
+            @RequestPart(value = Api.PARAM_USER_ID, required = true) String userId,
+            @RequestPart(value = Api.PARAM_FILE, required = true) Mono<FilePart> file) {
+
+        log.info("Got file! userId: "+userId+" file: "+file.block());
+
+        return Mono.create(sink -> {
+            sink.success(ResponseEntity.ok().build());
+        });
+    }
+
+    /**
+     * Изменить выбранное изображение
+     * @param userId String
+     * @param imageId String
+     * @param file Mono<FilePart>
+     * @return Mono<ResponseEntity<?>>
+     */
+    @PutMapping(value = Api.API_USER_IMAGES_USER_ID)
+    public Mono<ResponseEntity<?>> updateImage(
+            @RequestPart(value = Api.PARAM_USER_ID, required = true) String userId,
+            @RequestPart(value = Api.PARAM_IMAGE_ID, required = true) String imageId,
+            @RequestPart(value = Api.PARAM_FILE, required = true) Mono<FilePart> file) {
+
+        log.info("Got file! userId: "+userId+" file: "+" imageId: "+imageId+" file: "+file.block());
+
+        return Mono.create(sink -> {
+            sink.success(ResponseEntity.accepted().build());
+        });
+    }
+
+    /**
+     * Сохранить несколько изображений за раз
+     * @param userId String
+     * @param files Mono<FilePart>
+     * @return Mono<ResponseEntity<?>>
+     */
+    @PostMapping(value = Api.API_USER_IMAGES_SOME_USER_ID)
+    public Mono<ResponseEntity<?>> saveImages(
+            @RequestPart(value = Api.PARAM_USER_ID, required = true) String userId,
+            @RequestPart(value = Api.PARAM_FILES, required = true) Mono<FilePart> files) {
+
+        return Mono.create(sink -> {
+            sink.success(ResponseEntity.ok().build());
+        });
+    }
+}
