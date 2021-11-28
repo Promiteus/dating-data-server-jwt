de3fc03f5d51f0bd6001dbbe12ff3e43ea0e5ccd
Roman Matveev <r.matveev@socrat.marketing>
2021-11-28 16:19:52 +1000

    28.11.2021 -- Разработка сервиса сохранения/удаления файлов. Часть 5. Метод сохранения файла дополнен условиями проверки рабочих каталогов.

--- src/main/java/com/romanm/jwtservicedata/services/abstracts/StorageServiceBase.java
+++ src/main/java/com/romanm/jwtservicedata/services/abstracts/StorageServiceBase.java
@@ -7,10 +7,8 @@ import org.springframework.http.codec.multipart.FilePart;
 import org.springframework.util.FileSystemUtils;
 import reactor.core.publisher.Mono;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 
 @Slf4j
@@ -29,13 +27,15 @@ public class StorageServiceBase {
         }
     }
 
-    protected boolean dirExists(String dir) {
+    protected boolean isDirExists(String dir) {
         return Files.exists(Paths.get(dir));
     }
 
-    /*private Mono<Boolean> saveFileItem(FilePart filePart) {
+    private Mono<Boolean> saveFileItem(FilePart filePart, String userId) {
         return Mono.create(monoSink -> {
-            filePart.transferTo(this.fileDirectory).doOnSuccess(t -> {
+            String fileName = String.format(CommonConstants.MULTIMEDIA_DEST_DIR, this.baseDir, userId)+"/"+filePart.filename();
+
+            filePart.transferTo(Paths.get(fileName)).doOnSuccess(t -> {
                 log.info(MessageConstants.prefixMsg(String.format(MessageConstants.MSG_FILE_SAVED_SUCCESSFUL, filePart.filename())));
                 monoSink.success(true);
             }).doOnError(err -> {
@@ -43,7 +43,7 @@ public class StorageServiceBase {
                 monoSink.success(false);
             }).subscribe();
         });
-    }*/
+    }
 
     /**
      *
@@ -52,19 +52,26 @@ public class StorageServiceBase {
      * @return Mono<Boolean>
      */
     protected Mono<Boolean> save(Mono<FilePart> file, String userId) {
-        if (!this.dirExists(String.format(CommonConstants.MULTIMEDIA_DEST_DIR, this.baseDir, userId))) {
+        if (!this.isDirExists(this.baseDir)) {
+            this.createWorkDir(this.baseDir);
+        }
+
+        if (!this.isDirExists(String.format(CommonConstants.MULTIMEDIA_DEST_DIR, this.baseDir, userId))) {
             this.createWorkDir(String.format(CommonConstants.MULTIMEDIA_DEST_DIR, this.baseDir, userId));
         }
 
         return Mono.create(sink -> {
             file.doOnSuccess(filePart -> {
-                filePart.transferTo(Paths.get(String.format(CommonConstants.MULTIMEDIA_DEST_DIR, this.baseDir, userId)+"/"+filePart.filename()).toFile()).doOnSuccess(t -> {
+                String fileName = String.format(CommonConstants.MULTIMEDIA_DEST_DIR, this.baseDir, userId)+"/"+filePart.filename();
+
+                filePart.transferTo(Paths.get(fileName).toFile()).doOnSuccess(t -> {
                     log.info(MessageConstants.prefixMsg(String.format(MessageConstants.MSG_FILE_SAVED_SUCCESSFUL, filePart.filename())));
                     sink.success(true);
                 }).doOnError(err -> {
                     log.info(MessageConstants.prefixMsg(String.format(MessageConstants.MSG_ERR_FILE_SAVING, filePart.filename(), err.getMessage())));
                     sink.success(false);
                 }).subscribe();
+
             }).doOnError(err -> {
                 log.info(MessageConstants.errorPrefixMsg(err.getMessage()));
                 sink.success(false);
@@ -72,7 +79,7 @@ public class StorageServiceBase {
         });
     }
 
-    protected void deleteFile(String fileName, String userId) {
+    protected void deleteUserFile(String fileName, String userId) {
         try {
             Files.deleteIfExists(Paths.get(String.format(CommonConstants.MULTIMEDIA_DEST_DIR, this.baseDir, userId)+"/"+fileName));
         } catch (IOException e) {
@@ -83,6 +90,7 @@ public class StorageServiceBase {
     protected void deleteAllUserFiles(String fileName, String userId) {
         FileSystemUtils.deleteRecursively(Paths.get(String.format(CommonConstants.MULTIMEDIA_DEST_DIR, this.baseDir, userId)).toFile());
     }
+
 
     protected void deleteAll() {
         FileSystemUtils.deleteRecursively(Paths.get(this.baseDir).toFile());
