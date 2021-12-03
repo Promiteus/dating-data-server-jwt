041ab7ad9cc2d3d55994d6a7268d7b836fc69ee7
Roman Matveev <r.matveev@socrat.marketing>
2021-12-03 21:25:52 +1000

    03.12.2021 -- Тесты сохранения файлов. Пока не готовы.

---
+++ src/test/java/com/romanm/jwtservicedata/services/abstracts/StorageServiceBaseTest.java
@@ -0,0 +1,24 @@
+package com.romanm.jwtservicedata.services.abstracts;
+
+import org.junit.Test;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.client.MultipartBodyBuilder;
+import org.springframework.util.MultiValueMap;
+import reactor.core.publisher.Mono;
+
+public class StorageServiceBaseTest  {
+    private StorageServiceBase storageServiceBase = new StorageServiceBase("uploads_test");
+
+    @Test
+    public void saveFileTest() {
+       /* MultipartBodyBuilder multipartBodyBuilder = new MultipartBodyBuilder();
+        multipartBodyBuilder.part("partFile", new ClassPathResource("/test/file1.png"));
+
+        MultiValueMap<String, HttpEntity<?>> map = multipartBodyBuilder.build();
+
+
+
+        this.storageServiceBase.save(Mono.empty(), "200", 3);*/
+    }
+}
